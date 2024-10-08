7f08d0ca056f5199b0ae30666c38fe3bf4004f6c
Marko Janjic <marko.m.janjic@gmail.com>
2024-10-08 13:07:17 +0200

    refactored routes, controllers, and views

--- controllers/publicProduct.js
+++ controllers/publicProduct.js
@@ -1,23 +1,19 @@
 import models from "../db/query.js";
 
-const { Product, Category } = models;
+const { Product } = models;
 
 export const getAllProducts = async (req, res) => {
   const products = await Product.findAll({});
-  if (!req.user) {
-    return res.render("public/products", { products });
-  }
-  res.render("admin/productDash", { products });
+
+  res.render("public/products", { products });
 };
 
 export const productDetails = async (req, res) => {
   const { slug } = req.params;
   try {
     const product = await Product.findBySlug(slug);
-    if (!req.user) {
-      return res.status(200).render("public/productDetails", { product });
-    }
-    res.status(200).render("admin/productDetails", { product });
+
+    res.status(200).render("public/productDetails", { product });
   } catch (error) {
     console.log(error);
   }
